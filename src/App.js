dbd5131fc16b982bfd9df00b037cb812d9d38da8
Max281192 <erardmaxime@yahoo.fr>
2021-02-02 17:13:54 +0100

    react router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
 // Import Pages
 import AboutUs from './pages/AboutUs';
+import ContactUs from './pages/ContactUs';
+import OurWork from "./pages/OurWork";
 //Global Style
 import GlobalStyle from './components/GlobalStyle';
+// Import Nav
+import Nav from './components/Nav';
+// Routing
+import {Switch, Route} from 'react-router-dom';
 
 function App() {
-  return (
-    <div className="App">
-      <GlobalStyle />
-      <AboutUs />
-    </div>
-  );
+    return (
+        <div className="App">
+
+            <GlobalStyle/>
+
+            <Nav/>
+
+            <Switch>
+                <Route path="/" exact>
+                    <AboutUs/>
+                </Route>
+                <Route path="/work">
+                    <OurWork/>
+                </Route>
+                <Route path="/contact">
+                    <ContactUs/>
+                </Route>
+            </Switch>
+        </div>
+    );
 }
 
 export default App;
